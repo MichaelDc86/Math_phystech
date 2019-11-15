fd240116d73380d63cbb27d8c6cc36dae33f8e13
mike <lenskymiwa@ya.ru>
2019-11-15 16:01:41 +0300

    filled the matrix

--- C1W2/first.py
+++ C1W2/first.py
@@ -1,13 +1,37 @@
-uniq_words_list = []
+import numpy as np
+import pprint
+import re
+uniq_words_list_raw = []
+all_strings = []
+res_freq_list = []
 
-with open('sentences.txt', 'r') as file:
-    for row in file:
-        row = row.lower()
-        row_words_list = row.split()
-        for word_id in range(len(row_words_list)):
-            if row_words_list[word_id][-1] == ',':
-                row_words_list[word_id] = row_words_list[word_id][:-1:]
-            uniq_words_list.append(row_words_list[word_id])
-        list(set(uniq_words_list))
-        print(row)
-        print(row_words_list)
+
+def my_func():
+    with open('sentences.txt', 'r') as file:
+        for row in file:
+
+            row_words_list = re.split('[^a-z]', row.lower())
+            row_words_list = [x for x in row_words_list if x != '']
+            all_strings.append(row_words_list)
+            print(row_words_list)
+            for word in row_words_list:
+                if word not in uniq_words_list_raw:
+                    uniq_words_list_raw.append(word)
+            uniq_words_list = list(set(uniq_words_list_raw))
+
+    for string in all_strings:
+        list_for_arr = []
+        for word_id in range(len(uniq_words_list)):
+            frequency = row_words_list.count(uniq_words_list[word_id])
+            list_for_arr.append(frequency)
+
+        res_freq_list.append(list_for_arr)
+
+    matrix = np.array(res_freq_list)
+    pprint.pprint(matrix)
+    print(matrix.shape)
+    print(matrix[0])
+
+
+if __name__ == '__main__':
+    my_func()
